b6bea160e6e0f0a983299611dba991eaa5208993
jknowlestech <jonathan.knowles@ymail.com>
2022-11-18 11:57:44 -0800

    initial commit

---
+++ LikesProject/Likes Part II 2/script.js
@@ -0,0 +1,11 @@
+function changeText(){
+    document.querySelector('#likeMore').innerText ++;
+}
+
+function changeText2(){
+    document.querySelector('#likeMore2').innerText ++;
+}
+
+function changeText3(){
+    document.querySelector('#likeMore3').innerText ++;
+}
---
+++ Web Fundamentals Exam/script.js
@@ -0,0 +1,9 @@
+
+
+function newPic() {
+    document.querySelector("#succulent-1").src = "succulents-2.jpg";
+}
+
+function hide(element) {
+    element.remove();
+}
